b7e7249dc452f46c25535277ed791e969654769b
patruff <patruff@gmail.com>
2024-11-03 06:41:01 -0500

    more cleaning service

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,21 @@
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+// const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-module.exports = function override(config, env) {
-  if (env === 'production') {
-    // Find and remove the existing WorkboxWebpackPlugin
-    config.plugins = config.plugins.filter(
-      plugin => !(plugin instanceof WorkboxWebpackPlugin.InjectManifest)
-    );
+// module.exports = function override(config, env) {
+//   if (env === 'production') {
+//     // Find and remove the existing WorkboxWebpackPlugin
+//     config.plugins = config.plugins.filter(
+//       plugin => !(plugin instanceof WorkboxWebpackPlugin.InjectManifest)
+//     );
 
-    // Add our custom WorkboxWebpackPlugin configuration
-    config.plugins.push(
-      new WorkboxWebpackPlugin.InjectManifest({
-        swSrc: './src/service-worker.js',
-        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024, // 50MB
-        dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
-      })
-    );
-  }
+//     // Add our custom WorkboxWebpackPlugin configuration
+//     config.plugins.push(
+//       new WorkboxWebpackPlugin.InjectManifest({
+//         swSrc: './src/service-worker.js',
+//         maximumFileSizeToCacheInBytes: 50 * 1024 * 1024, // 50MB
+//         dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
+//       })
+//     );
+//   }
 
-  return config;
-} 
+//   return config;
+// } 
