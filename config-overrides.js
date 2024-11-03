be19844b1d0fee1ebb07af735e89a1bc68426fe9
patruff <patruff@gmail.com>
2024-11-03 05:39:28 -0500

    more service worker errors

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+module.exports = function override(config, env) {
+  if (env === 'production') {
+    // Find and remove the existing WorkboxWebpackPlugin
+    config.plugins = config.plugins.filter(
+      plugin => !(plugin instanceof WorkboxWebpackPlugin.InjectManifest)
+    );
+
+    // Add our custom WorkboxWebpackPlugin configuration
+    config.plugins.push(
+      new WorkboxWebpackPlugin.InjectManifest({
+        swSrc: './src/service-worker.js',
+        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024, // 50MB
+        dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
+      })
+    );
+  }
+
+  return config;
+} 
